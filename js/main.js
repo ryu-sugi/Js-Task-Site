ccb4b0cd3bad99f9a8d59b5dc9f4f035d9f49e7f
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-08-31 12:00:42 +0900

    ハンバーガーメニューに関する記述

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
  // エラーチェック厳密化の記述
 'use strict'
- 
+
+{
+ // アコーディオンメニューに関する記述
  const menu = document.querySelectorAll(".js-menu"); // クリックする3つの要素js-menuというクラスを取得
 
  function toggle() { // toggle関数を定義(toggleはクラスを付けたり外したりしてくれる。)
@@ -13,3 +15,29 @@
 for (let i = 0; i < menu.length; i++) { // for文で要素にクリックイベントをつける
       menu[i].addEventListener("click",toggle);
     }
+}
+ // アコーディオンメニューに関する記述
+
+
+// ハンバーガーメニューに関する記述
+{
+const open = document.getElementById("open");
+
+ // querySelectorを使用する事でCSSセレクタを取得してくる
+ const overlay = document.querySelector('.overlay');
+
+ // HTMLから'close'IDを取得してくる
+ const close = document.getElementById('close');
+
+ // クリックする事でイベント発火させる為の記述
+ open.addEventListener('click', () => {
+   overlay.classList.add('show'); //overlayのshowクラスを指定
+   open.classList.add('hide'); //openのhideクラスを指定
+ });
+
+ close.addEventListener('click', () => {
+  overlay.classList.remove('show'); 
+  open.classList.remove('hide'); 
+});
+}
+// ハンバーガーメニューに関する記述
